34cccbb4970af1e7bf41e4d99df3e11155a82644
Gabriel-Cassimiro <gabrielguedes06@gmail.com>
2021-08-13 17:11:45 -0300

    Added Cart component in project and Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        orange: colors.orange,
+      },
       gridTemplateRows: {
         layout: " 0.4fr 0.1fr 0.1fr 0.2fr 0.2fr"
       },
